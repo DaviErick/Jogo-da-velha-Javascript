c8d3c644bf5e54e4eda047e2a82526bb772fd845
DaviErick <davierick999@gmail.com>
2023-05-05 14:42:17 -0300

    versao 2
    versao 2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-script.js
+var numerosIndisponiveis = [];
+function clicar(num, posicao) {
+    num.innerHTML = 'X';
+    numerosIndisponiveis.push(posicao)
+    oponente();
+}
+console.log(numerosIndisponiveis);
+
+function oponente(){
+	var numeroAleatorio = Math.floor(Math.random() * 9) + 1;
+	numerosIndisponiveis.push(numeroAleatorio);
+	alert(numeroAleatorio);
+	var o = document.querySelector('.n' + numeroAleatorio);
+	o.innerHTML = 'O';
+
+}
