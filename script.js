c957acde7bc9c7970d649597db44926f5ae219d0
DaviErick <davierick999@gmail.com>
2023-05-06 12:20:28 -0300

    Projeto atualizado v1
    Projeto atualizado v1

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 var numerosIndisponiveis = [];
+
 function clicar(num, posicao) {
-    num.innerHTML = 'X';
-    numerosIndisponiveis.push(posicao)
-    oponente();
+  num.innerHTML = 'X';
+  oponente();
 }
-console.log(numerosIndisponiveis);
 
 function oponente(){
 	var numeroAleatorio = Math.floor(Math.random() * 9) + 1;
 	numerosIndisponiveis.push(numeroAleatorio);
 	alert(numeroAleatorio);
-	var o = document.querySelector('.n' + numeroAleatorio);
 	o.innerHTML = 'O';
 
+function oponente() {
+  var numeroAleatorio;
+  do {
+    numeroAleatorio = Math.floor(Math.random() * 9) + 1;
+  } while (numerosIndisponiveis.includes(numeroAleatorio));
+  
+  numerosIndisponiveis.push(numeroAleatorio);
+  alert(numeroAleatorio);
+  var o = document.querySelector('.n' + numeroAleatorio);
+  o.innerHTML = 'O';
 }
